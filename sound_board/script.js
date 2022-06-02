784ea17201bb7ba3e3cf310732d7810e99e84345
blatnak <katerina.blatna@uzis.cz>
2022-06-02 11:03:20 +0200

    Funciton changes to stop play after click on the frog

--- sound_board/script.js
+++ sound_board/script.js
@@ -1,42 +1,59 @@
 const frogs = [
-    {
-        "name": "Boreal Chorus Frog",
-        "url": "http://www.naturenorth.com/spring/sound/Frog_B_Chorus.jpg",
-    },
-    {
-        "name": "Copes Gray Treefrog",
-        "url": "http://www.naturenorth.com/spring/sound/Treefrog_Gray.jpg",
-    },
-    {
-        "name": "Great Plains Toad",
-        "url": "http://www.naturenorth.com/spring/sound/Toad_Gr_Plains.jpg",
-    },
-    {
-        "name": "Green Frog",
-        "url": "http://www.naturenorth.com/spring/sound/Frog_Green.jpg",
-    },
-    {
-        "name": "Leopard Frog",
-        "url": "http://www.naturenorth.com/spring/sound/Frog_Leopard.jpg",
-    },
-    {
-        "name": "Mink Frog",
-        "url": "http://www.naturenorth.com/spring/sound/Frog_Mink.jpg",
-    },
+  {
+    name: 'Boreal Chorus Frog',
+    url: 'http://www.naturenorth.com/spring/sound/Frog_B_Chorus.jpg',
+  },
+  {
+    name: 'Copes Gray Treefrog',
+    url: 'http://www.naturenorth.com/spring/sound/Treefrog_Gray.jpg',
+  },
+  {
+    name: 'Great Plains Toad',
+    url: 'http://www.naturenorth.com/spring/sound/Toad_Gr_Plains.jpg',
+  },
+  {
+    name: 'Green Frog',
+    url: 'http://www.naturenorth.com/spring/sound/Frog_Green.jpg',
+  },
+  {
+    name: 'Leopard Frog',
+    url: 'http://www.naturenorth.com/spring/sound/Frog_Leopard.jpg',
+  },
+  {
+    name: 'Mink Frog',
+    url: 'http://www.naturenorth.com/spring/sound/Frog_Mink.jpg',
+  },
 ]
 
 frogs.forEach((frog) => {
   const btn = document.createElement('button') // create button
   btn.classList.add('btn') // add class for this button
   btn.innerHTML = frog.name //add name of frog from frogs array to button text
-  
-  const img = document.createElement("img");
-  btn.appendChild(img)
-  img.src = frog.url
+
+
+  const img = document.createElement('img') // add img element
+  btn.appendChild(img) // add img to btn
+  img.src = frog.url //add url to img source
+
+
+  let frogSound = document.getElementById(frog.name) // find the specific frog
+  let isPlaying = false // default state 
+
+  function togglePlay() { // toggle between states
+    isPlaying ? frogSound.pause() : frogSound.play()
+  }
+
+
+  frogSound.onplaying = () => { //change the state
+    isPlaying = true
+  }
+  frogSound.onpause = () => {
+    isPlaying = false
+  }
 
   btn.addEventListener('click', () => {
-    stopPlay() // before start the sound we have to stop the previous sound
-    document.getElementById(frog.name).play() // find concrete frog by unique id and play the sound
+    stopPlay()
+    togglePlay()
   })
 
   document.getElementById('buttons').appendChild(btn) // find div and added all buttons in it
