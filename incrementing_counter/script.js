280eb0d0d56a3fe7a1fa278f7688d58cae8bfb25
blatnak <katerina.blatna@uzis.cz>
2022-06-08 13:35:31 +0200

    function update with comments

--- incrementing_counter/script.js
+++ incrementing_counter/script.js
@@ -3,15 +3,16 @@ const counters = document.querySelectorAll('.counter')
 counters.forEach((counter) => {
   counter.innerText = 0 // Due to the innertext, it is a string, so we need to make a number from this in the line 10
 
-  const updateCounter = () => {
+  //const updateCounter = () => { It is also possible to have normal function here
+  function updateCounter() {
     let target = Number(counter.getAttribute('data-target'))
     let value = Number(counter.innerText)
 
-    const increment = target / 200
+    const increment = target / 200 // split target by same number to have same start point
 
     if (value < target) {
       counter.innerText = value + increment // number + number, but due to innerText it is a string again
-      setTimeout(updateCounter, 1)
+      setTimeout(updateCounter, 1) // call the function with timeout
     } else {
       counter.innerText = target
     }
