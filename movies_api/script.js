d4c1c686386ecc11e5fce23bd9609f5ac9764adc
blatnak <katerina.blatna@uzis.cz>
2022-06-20 15:48:35 +0200

    WIP - fetching data from api, push into html

---
+++ movies_api/script.js
@@ -0,0 +1,75 @@
+const API_KEY = '7713bb2f56de7e4bdcaac26b537f1464&page=1'
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=10`
+const IMG_URL = 'https://image.tmdb.org/t/p/w1280' //https://developers.themoviedb.org/3/getting-started/images
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+// Get initial movies
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+  //console.log(data) // vrátí komplet všechno
+  showMovies(data.results) // vrátí už jen výsledky, s informacemi, které nás zajímají
+}
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    poster_path === null
+
+    if(poster_path === null) {
+        poster_path = "test"
+    }
+
+    movieEl.innerHTML = 
+       `<img
+          src="${IMG_URL + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`
+
+      main.appendChild(movieEl)
+       
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_URL + searchTerm)
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
