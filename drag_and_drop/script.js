cf25f9ff0044f6f618b2bd14467545ef5febd35e
blatnak <katerina.blatna@uzis.cz>
2022-07-21 13:57:46 +0200

    Documentation added

--- drag_and_drop/script.js
+++ drag_and_drop/script.js
@@ -1,3 +1,5 @@
+// Documentation: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+
 const filledEl = document.querySelector('.fill')
 const emptyItems = document.querySelectorAll('.empty')
 
